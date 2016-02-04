a6e32d57dda32d03c49571061a36f7d54411c049
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2016-02-04 18:37:48 +0200

    Initial Commit

---
+++ Experimental-UI-Course-iOS-AppTests/Experimental_UI_Course_iOS_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Experimental_UI_Course_iOS_AppTests.swift
+//  Experimental-UI-Course-iOS-AppTests
+//
+//  Created by Toni Antero Karttunen on 04/02/16.
+//  Copyright © 2016 Toni Antero Karttunen. All rights reserved.
+//
+
+import XCTest
+@testable import Experimental_UI_Course_iOS_App
+
+class Experimental_UI_Course_iOS_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
