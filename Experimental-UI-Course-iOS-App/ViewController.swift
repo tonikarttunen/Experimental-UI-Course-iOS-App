6bcf38798cfbe300b8a661564eef2771181acc49
Toni Antero Karttunen <toni.antero.karttunen@gmail.com>
2016-02-09 15:27:47 +0200

    Install Alamofire with CocoaPods

--- Experimental-UI-Course-iOS-App/ViewController.swift
+++ Experimental-UI-Course-iOS-App/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.greenColor()
+        
+        for i in 0...21 {
+            print(i)
+            Alamofire.request(.POST, "http://172.20.10.2:5000")
+                .responseData { response in
+                    print(response.request)
+                    print(response.response)
+                    print(response.result)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
